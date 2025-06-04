c76c642f1c0c65e1591844807a26f91f43451024
Cleyson Silva <cleysonsilva.me@gmail.com>
2025-06-04 02:18:05 -0300

    refactor: update project configuration files and dependencies for ai-personal-coach

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,12 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
+    },
+  },
+  build: {
+    sourcemap: true,
+  }
 });
