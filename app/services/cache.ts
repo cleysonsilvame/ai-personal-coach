db8d4ebff0ae8558f590b23d86188680c2b56e08
Cleyson Silva <cleysonsilva.me@gmail.com>
2025-06-08 18:43:24 -0300

    chore(cache): comment out Redis initialization and replace with empty object for cache

--- app/services/cache.ts
+++ app/services/cache.ts
@@ -2,4 +2,6 @@ import Redis from "ioredis";
 
 export const TTL = 60 * 60 * 24;
 
-export const cache = new Redis(process.env.UPSTASH!);
+// export const cache = new Redis(process.env.UPSTASH!); // TODO: add upstash
+
+export const cache = {};
