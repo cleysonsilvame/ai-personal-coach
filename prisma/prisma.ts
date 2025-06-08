b4d7507bddf61edefce447ee240d9ad2221a4b66
Cleyson Silva <cleysonsilva.me@gmail.com>
2025-06-08 18:43:00 -0300

    chore(prisma): reorganize imports and update global prismaClient declaration

--- prisma/prisma.ts
+++ prisma/prisma.ts
@@ -1,15 +1,15 @@
-import "dotenv/config";
+import "dotenv/config"; // TODO: move to lib/prisma/client.ts
 
-import { PrismaClient } from "../app/generated/prisma";
 import { PrismaLibSQL } from "@prisma/adapter-libsql";
+import { PrismaClient } from "../app/generated/prisma";
 
 declare global {
-  var prismaClient: PrismaClient;
+	var prismaClient: PrismaClient;
 }
 
 const adapter = new PrismaLibSQL({
-  url: `${process.env.TURSO_DATABASE_URL}`,
-  authToken: `${process.env.TURSO_AUTH_TOKEN}`,
+	url: `${process.env.TURSO_DATABASE_URL}`,
+	authToken: `${process.env.TURSO_AUTH_TOKEN}`,
 });
 
 globalThis.prismaClient ??= new PrismaClient({ adapter });
