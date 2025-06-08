8b0c202e2e387d8a4985c6556d5d2311daf3b506
Cleyson Silva <cleysonsilva.me@gmail.com>
2025-06-08 18:44:44 -0300

    refactor(layout): improve layout component structure and add TODO for sidebar state management

--- app/layouts/layout.tsx
+++ app/layouts/layout.tsx
@@ -5,24 +5,24 @@ import { Outlet } from "react-router";
 import { SiteHeader } from "~/components/site-header";
 
 export default function () {
-  return (
-    <SidebarProvider
-      style={
-        {
-          "--sidebar-width": "calc(var(--spacing) * 72)",
-          "--header-height": "calc(var(--spacing) * 12)",
-        } as React.CSSProperties
-      }
-    >
-      <AppSidebar variant="inset" />
-      <SidebarInset>
-        <SiteHeader />
-        <div className="flex flex-1 flex-col">
-          <div className="@container/main flex flex-1 flex-col gap-2">
-            <Outlet />
-          </div>
-        </div>
-      </SidebarInset>
-    </SidebarProvider>
-  );
+	return (
+		<SidebarProvider // TODO: save sidebar state on storage (cookies or local storage)
+			style={
+				{
+					"--sidebar-width": "calc(var(--spacing) * 72)",
+					"--header-height": "calc(var(--spacing) * 12)",
+				} as React.CSSProperties
+			}
+		>
+			<AppSidebar variant="inset" />
+			<SidebarInset>
+				<SiteHeader />
+				<div className="flex flex-1 flex-col">
+					<div className="@container/main flex flex-1 flex-col gap-2">
+						<Outlet />
+					</div>
+				</div>
+			</SidebarInset>
+		</SidebarProvider>
+	);
 }
