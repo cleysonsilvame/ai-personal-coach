3a647320763371b78d19270f6c51eac3fa38e06a
Cleyson Silva <cleysonsilva.me@gmail.com>
2025-06-09 02:57:15 -0300

    chore(vite): update vite.config.ts to conditionally include Tabler icons import based on command

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,16 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
 	plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 	resolve: {
 		alias: {
-			// "@tabler/icons-react": "@tabler/icons-react/dist/esm/icons/index.mjs", TODO: fix this
+			...(command === "serve"
+				? {
+						"@tabler/icons-react":
+							"@tabler/icons-react/dist/esm/icons/index.mjs", // TODO: temp: https://github.com/tabler/tabler-icons/issues/1233#issuecomment-2428245119
+					}
+				: {}),
 		},
 	},
-});
+}));
