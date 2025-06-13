141d0af340f53a8745574206fcbf4f90754ac742
Cleyson Silva <cleysonsilva.me@gmail.com>
2025-06-13 01:23:35 -0300

    feat(container): bind ChatService to OpenRouterChatService in the dependency injection container

--- app/lib/container.ts
+++ app/lib/container.ts
@@ -1,11 +1,14 @@
 import { Container } from "inversify";
 import { PrismaChatRepository } from "~/database/chat.repository";
 import { ChatRepository } from "~/features/goals/repositories/chat";
+import { ChatService } from "~/features/goals/services/chat";
+import { OpenRouterChatService } from "~/services/chat.server";
 
 const container = new Container({
 	autobind: true,
 });
 
 container.bind(ChatRepository).to(PrismaChatRepository).inSingletonScope();
+container.bind(ChatService).to(OpenRouterChatService).inSingletonScope();
 
 export { container };
