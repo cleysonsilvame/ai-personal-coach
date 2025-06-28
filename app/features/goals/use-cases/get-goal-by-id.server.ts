852c0a6f4e9b5aa3470e8670ded500e4bb38f382
Cleyson Silva <cleysonsilva.me@gmail.com>
2025-06-28 20:21:11 -0300

    refactor(GetGoalByIdUseCase): update execute method to accept similar parameter and adjust return type for similarGoals

--- app/features/goals/use-cases/get-goal-by-id.server.ts
+++ app/features/goals/use-cases/get-goal-by-id.server.ts
@@ -3,12 +3,12 @@ import { GoalRepository } from "../repositories/goal";
 import type { Goal } from "../entities/goal";
 import { GoalCacheService } from "../services/cache";
 import type { SimilarGoal } from "../entities/similar-goal";
-import type { EmbeddingService } from "../services/embedding";
-import type { GoalEmbeddingRepository } from "../repositories/goal-embedding";
+import { EmbeddingService } from "../services/embedding";
+import { GoalEmbeddingRepository } from "../repositories/goal-embedding";
 
 interface GetGoalsByIdOutput {
 	goal: Goal;
-	similarGoals: SimilarGoal[];
+	similarGoals: SimilarGoal[] | null;
 }
 
 @injectable()
@@ -18,15 +18,21 @@ export class GetGoalByIdUseCase {
 		private readonly goalRepository: GoalRepository,
 		@inject(GoalCacheService)
 		private readonly goalsCacheService: GoalCacheService,
+		@inject(EmbeddingService)
 		private readonly embeddingsService: EmbeddingService,
+		@inject(GoalEmbeddingRepository)
 		private readonly goalsEmbeddingRepository: GoalEmbeddingRepository,
 	) {}
 
-	async execute(goalId: string): Promise<GetGoalsByIdOutput> {
+	async execute(goalId: string, similar = false): Promise<GetGoalsByIdOutput> {
 		const goal = await this.goalRepository.findById(goalId);
 
 		if (!goal) {
 			throw new Error("Goal not found");
+		}
+
+		if (!similar) {
+			return { goal, similarGoals: null };
 		}
 
 		let similarGoals = await this.goalsCacheService.getSimilarGoals(goalId);
