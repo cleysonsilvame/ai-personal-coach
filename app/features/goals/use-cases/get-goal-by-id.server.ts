90968766a5a2488abc539e6ab0641bb483d4f2b2
Cleyson Silva <cleysonsilva.me@gmail.com>
2025-06-28 19:50:05 -0300

    feat(goals): update GetGoalByIdUseCase to return goal with similar goals and integrate caching for improved performance

--- app/features/goals/use-cases/get-goal-by-id.server.ts
+++ app/features/goals/use-cases/get-goal-by-id.server.ts
@@ -1,23 +1,50 @@
 import { inject, injectable } from "inversify";
 import { GoalRepository } from "../repositories/goal";
 import type { Goal } from "../entities/goal";
+import { GoalCacheService } from "../services/cache";
+import type { SimilarGoal } from "../entities/similar-goal";
+import type { EmbeddingService } from "../services/embedding";
+import type { GoalEmbeddingRepository } from "../repositories/goal-embedding";
+
+interface GetGoalsByIdOutput {
+	goal: Goal;
+	similarGoals: SimilarGoal[];
+}
 
 @injectable()
 export class GetGoalByIdUseCase {
 	constructor(
 		@inject(GoalRepository)
 		private readonly goalRepository: GoalRepository,
+		@inject(GoalCacheService)
+		private readonly goalsCacheService: GoalCacheService,
+		private readonly embeddingsService: EmbeddingService,
+		private readonly goalsEmbeddingRepository: GoalEmbeddingRepository,
 	) {}
 
-	async execute(goalId: string): Promise<Goal> {
+	async execute(goalId: string): Promise<GetGoalsByIdOutput> {
 		const goal = await this.goalRepository.findById(goalId);
 
 		if (!goal) {
 			throw new Error("Goal not found");
 		}
 
-		// const similarGoals = await findSimilarGoals(goal.title, 4);
+		let similarGoals = await this.goalsCacheService.getSimilarGoals(goalId);
 
-		return goal;
+		if (!similarGoals) {
+			const embedding = await this.embeddingsService.createEmbeddingFromTitle(
+				goal.title,
+			);
+
+			similarGoals = await this.goalsEmbeddingRepository.findSimilar(
+				embedding,
+				goalId,
+				4,
+			);
+
+			await this.goalsCacheService.setSimilarGoals(goalId, similarGoals);
+		}
+
+		return { goal, similarGoals };
 	}
 }
