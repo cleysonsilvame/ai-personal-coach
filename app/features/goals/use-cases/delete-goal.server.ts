5ce0249f95663a6c81cec88e3cd63dd4733ccdac
Cleyson Silva <cleysonsilva.me@gmail.com>
2025-06-29 16:23:13 -0300

    refactor(goals): enhance DeleteGoalUseCase with transaction management and cache invalidation for improved data integrity

--- app/features/goals/use-cases/delete-goal.server.ts
+++ app/features/goals/use-cases/delete-goal.server.ts
@@ -1,15 +1,24 @@
 import { inject, injectable } from "inversify";
+import { UnitOfWork } from "~/features/core/services/unit-of-work";
 import { GoalRepository } from "../repositories/goal";
+import { GoalCacheService } from "../services/cache";
 
 @injectable()
 export class DeleteGoalUseCase {
 	constructor(
 		@inject(GoalRepository)
 		private readonly goalRepository: GoalRepository,
+		@inject(GoalCacheService)
+		private readonly goalCacheService: GoalCacheService,
+		@inject(UnitOfWork)
+		private readonly uow: UnitOfWork,
 	) {}
 
 	async execute(goalId: string): Promise<void> {
-		await this.goalRepository.deleteById(goalId);
-		// TODO: delete cache e adicionar transaction
+		await this.uow.execute(async (tx) => {
+			const transactionalGoalRepo = this.goalRepository.setTransaction(tx);
+			await transactionalGoalRepo.deleteById(goalId);
+			await this.goalCacheService.invalidateAllSimilarGoals();
+		});
 	}
 }
