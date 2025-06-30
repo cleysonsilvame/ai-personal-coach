379cd4cd03acfc5a54df0dd634aba0b9d4eeaf24
Cleyson Silva <cleysonsilva.me@gmail.com>
2025-06-30 19:44:37 -0300

    refactor(unit-of-work): simplify execute method by removing optional parameters

--- app/features/core/services/unit-of-work.ts
+++ app/features/core/services/unit-of-work.ts
@@ -3,8 +3,5 @@ import type { Transaction } from "./transaction";
 
 @injectable()
 export abstract class UnitOfWork {
-	abstract execute<T>(
-		fn: (tx: Transaction) => Promise<T>,
-		options?: { timeout: number; maxWait?: number },
-	): Promise<T>;
+	abstract execute<T>(fn: (tx: Transaction) => Promise<T>): Promise<T>;
 }
